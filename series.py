75638b7f34d1ee8900f011953f08d87daa9bdc3e
Scottie <scott.c.houghton@gmail.com>
2022-07-19 18:33:21 -0700

    Made three Lucas tests. Modified the Lucas function so that it would pass one test, but fail two others

--- series.py
+++ series.py
@@ -2,7 +2,7 @@ def fibonacci(n):
     return 5
 
 def lucas(n):
-    return 0
+    return 7
 
 def sum_series(n):
     return 0
--- test_series.py
+++ test_series.py
@@ -16,3 +16,18 @@ def test_fibonacci_index_8():
     actual = fibonacci(8)
     expected = 13
     assert actual == expected
+
+def test_lucas_index_5():
+    actual = lucas(5)
+    expected = 7
+    assert actual == expected
+
+def test_lucas_index_6():
+    actual = lucas(6)
+    expected = 11
+    assert actual == expected
+
+def test_lucas_index_7():
+    actual = lucas(7)
+    expected = 18
+    assert actual == expected
