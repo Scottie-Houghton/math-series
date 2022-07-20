df634a3a463a698ecdc8c03dff5a3508bda82ccd
Scottie <scott.c.houghton@gmail.com>
2022-07-19 18:02:39 -0700

    Got functions ready in the series module. Got the imports ready in the test file.

---
+++ series.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    return 0
+
+def lucas(n):
+    return 0
+
+def sum_series(n):
+    return 0
---
+++ test_series.py
@@ -0,0 +1,4 @@
+from series import fibonacci
+from series import lucas
+from series import sum_series
+
