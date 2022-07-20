0719fd5efabfe16f92dc8eae18140401b3cf2c96
Scottie <scott.c.houghton@gmail.com>
2022-07-19 18:27:45 -0700

    Installed pytest. Made two more test functions. Modified Fibonacci function so that it would pass the first test, but fail the two others.

--- test_series.py
+++ test_series.py
@@ -6,3 +6,13 @@ def test_fibonacci_index_6():
     actual = fibonacci(6)
     expected = 5
     assert actual == expected
+
+def test_fibonacci_index_7():
+    actual = fibonacci(7)
+    expected = 8
+    assert actual == expected
+
+def test_fibonacci_index_8():
+    actual = fibonacci(8)
+    expected = 13
+    assert actual == expected
