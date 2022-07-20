e724f87343451b84a57ebc6598cdf845e3587ff6
Scottie <scott.c.houghton@gmail.com>
2022-07-19 19:13:12 -0700

    Realized my first two tests for both Fibonacci and Lucas were wrong, so fixed them. Built out the Lucas function and made sure it passed all 3 of its tests.

--- series.py
+++ series.py
@@ -7,7 +7,12 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 def lucas(n):
-    return 7
+    if n == 1:
+        return 2
+    elif n == 2:
+        return 1
+    else:
+        return lucas(n-1) + lucas(n-2)
 
 def sum_series(n):
     return 0
--- test_series.py
+++ test_series.py
@@ -2,14 +2,14 @@ from series import fibonacci
 from series import lucas
 from series import sum_series
 
-def test_fibonacci_index_6():
-    actual = fibonacci(6)
-    expected = 5
+def test_fibonacci_index_1():
+    actual = fibonacci(1)
+    expected = 0
     assert actual == expected
 
-def test_fibonacci_index_7():
-    actual = fibonacci(7)
-    expected = 8
+def test_fibonacci_index_2():
+    actual = fibonacci(2)
+    expected = 1
     assert actual == expected
 
 def test_fibonacci_index_8():
@@ -17,14 +17,14 @@ def test_fibonacci_index_8():
     expected = 13
     assert actual == expected
 
-def test_lucas_index_5():
-    actual = lucas(5)
-    expected = 7
+def test_lucas_index_1():
+    actual = lucas(1)
+    expected = 2
     assert actual == expected
 
-def test_lucas_index_6():
-    actual = lucas(6)
-    expected = 11
+def test_lucas_index_2():
+    actual = lucas(2)
+    expected = 1
     assert actual == expected
 
 def test_lucas_index_7():
